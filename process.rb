8a48049b9ac4bf953ac6dc775e98f7b6a5d09bb7
Mike Solomon <msolomon@thrillist.com>
2015-11-09 15:31:01 -0500

    debug

--- process.rb
+++ process.rb
@@ -195,7 +195,9 @@ def start_progress (jira_issues, user, *branch)
 		end
 	else
 		while (i < jira_issues.length) do
-			jira_issue = jira_issues[i].join
+			jira_issue = jira_issues[i]
+			puts "jira issue"
+			puts jira_issue
 			transition_issue jira_issue, START_PROGRESS_ID, user
 			i+=1
 		end
